cf827ae4e37d8dae52e02b2078a81d70ade0db93
ARena18 <91445893+ARena18@users.noreply.github.com>
2023-10-29 00:56:56 -0700

    Update Setup.js

--- Setup.js
+++ Setup.js
@@ -1,58 +1,78 @@
-//File with setup and initialization code
+//Epidemiology Simulation Project: Anna Muller,
+//                                 Rena Ahn
 //To be merged with Epidemiology.js
 
 import file from "./config.json" with { type: "json"};
 
-//function: creating and appending grid of 'canvas' elements
-var container = document.getElementById("grid");
-container.style.height = (simulation.gridHeight * 52) + 'px';
-container.style.width = (simulation.gridWidth * 52) + 'px';
+//declaring simulation variables
+var gridInput = document.getElementById("gridSlider");
+var maskInput = document.getElementById("maskText");
+var vaccInput = document.getElementById("vaccText");
+var submitButton = document.getElementById("submitBtn");
 
- for (var x = 0; x < simulation.gridHeight; x++) for (var y = 0; y < simulation.gridWidth; y++) {
-   let div = document.createElement("canvas");
-   var tempStr = `${x} + ${y}`;
-   div.id = tempStr;
-   div.height = 50;
-   div.width = 50;
-   container.appendChild(div);
-}
+submitButton.addEventListener("click", redrawGrid, true);
 
-//temp: 2D array determining infection
-let arr = [];
-for (var i = 0; i < simulation.gridHeight; i++) {
-  arr[i] = [];
-}
+function drawGrid(height, width) {
+  //function: creating and appending grid of 'canvas' elements
+  var container = document.getElementById("grid");
+  container.style.height = (height * 52) + 'px';
+  container.style.width = (width * 52) + 'px';
 
-//randomly assigning which people are infected
-for (var i = 0; i < 100; i++) {
-  var x = Math.floor(Math.random() * simulation.gridHeight);
-  var y = Math.floor(Math.random() * simulation.gridWidth);
-  while(arr[x][y] !== undefined) {
-    x = Math.floor(Math.random() * simulation.gridHeight);
-    y = Math.floor(Math.random() * simulation.gridWidth);
+  for (var x = 0; x < height; x++) for (var y = 0; y < width; y++) {
+    let div = document.createElement("canvas");
+    var tempStr = `${x} + ${y}`;
+    div.id = tempStr;
+    div.height = 50;
+    div.width = 50;
+    container.appendChild(div);
   }
-  arr[x][y] = new Person;
-}
 
-//drawing circles for each person
-for (var i = 0; i < simulation.gridHeight; i++) {
-  for (var j = 0; j < simulation.gridWidth; j++) {
-    if(arr[i][j] === undefined) {
-      continue;
+  //temp: 2D array determining infection
+  let arr = [];
+  for (var i = 0; i < height; i++) {
+    arr[i] = [];
+  }
+
+  //randomly assigning which people are infected
+  for (var i = 0; i < 100; i++) {
+    var x = Math.floor(Math.random() * height);
+    var y = Math.floor(Math.random() * width);
+    while (arr[x][y] !== undefined) {
+      x = Math.floor(Math.random() * height);
+      y = Math.floor(Math.random() * width);
     }
-    var tempStr = `${i} + ${j}`
-    var canvas = document.getElementById(tempStr);
-    var context = canvas.getContext("2d");
-    if(arr[i][j].immune) {
-      context.fillStyle = "yellow";
-    } else if (arr[i][j].infected) {
-      context.fillStyle = "red";
+    arr[x][y] = new Person;
+  }
+
+  //drawing circles for each person
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      if (arr[i][j] === undefined) {
+        continue;
+      }
+      var tempStr = `${i} + ${j}`
+      var canvas = document.getElementById(tempStr);
+      var context = canvas.getContext("2d");
+      if (arr[i][j].immune) {
+        context.fillStyle = "yellow";
+      } else if (arr[i][j].infected) {
+        context.fillStyle = "red";
+      } else {
+        context.fillStyle = "blue";
+      }
+      context.beginPath();
+      context.arc(25, 25, 20, 0, 2 * Math.PI);
+      context.fill();
     }
-    else {
-      context.fillStyle = "blue";
-    }
-    context.beginPath();
-    context.arc(25, 25, 20, 0, 2 * Math.PI);
-    context.fill();
   }
 }
+
+function redrawGrid() {
+  var container = document.getElementById("grid");
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  drawGrid(gridInput.value, gridInput.value);
+}
+
+drawGrid(gridInput.value, gridInput.value);
