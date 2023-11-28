dfd2250f698cddbc45b8fe055e40348ad693c78f
ARena18 <91445893+ARena18@users.noreply.github.com>
2023-11-27 16:45:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Epidemiology Simulation Project: Rena Ahn and Anna Muller
-//Merged with Epidemiology.js [last update: 11/21/2023]
+//Merged with Epidemiology.js [last update: 11/27/2023]
 //   Improvement Goals...
 //   (1) Proceeding through the simulation automatically or being able to look at desired (UI) days
-//   (2) Design/Choose then implement a commenting style
+//   (2) Implement a commenting style
 
 //Person class
 class Person {
@@ -45,17 +45,17 @@ class Person {
 
 //Disease class
 class Disease {
-  constructor(transmissionFactor, vaccEff, daysToSx, daysToImmune) {
+  constructor(transmissionFactor, vaccineEfficacy, daysToSymptoms, daysToImmune) {
     this.transmissionFactor = transmissionFactor;
-    this.vaccEff = vaccEff;
-    this.daysToSx = daysToSx;
+    this.vaccineEfficacy = vaccineEfficacy;
+    this.daysToSymptoms = daysToSymptoms;
     this.daysToImmune = daysToImmune;
   }
 }
 const diseaseDictionary = {
   "leastInfectious": new Disease(20, 40, 2, 12), // covid inspired
-  "mediumInfectious": new Disease(30, 60, 7, 14), // rubella inspired
-  "mostInfectious": new Disease(40, 80, 4, 8) // measles inspired    
+  "mediumInfectious": new Disease(30, 30, 7, 14), // rubella inspired
+  "mostInfectious": new Disease(40, 35, 4, 8) // measles inspired    
 }
 
 // Desc : Depending on the setting, number of people who wear masks
@@ -182,7 +182,7 @@ const simulation = {
   "populationSize": 100,
   "disease": diseaseDictionary.leastInfectious,
   "maskLevel": maskDictionary.mediumMask,
-  "maskProtection": 10,
+  "maskProtection": 20,
   "vaccLevel": vaccineDictionary.mediumVacc
 }
 
@@ -219,8 +219,8 @@ function assignMasks(maskedPeople) {
 function assignVacc(vaccPeople) {
   for (var i = 0; i < vaccPeople.length; i++) {
     vaccPeople[i].vaccine = true;
-    vaccPeople[i].protection += simulation.disease.vaccEff;
-    vaccPeople[i].transmission -= simulation.disease.vaccEff;
+    vaccPeople[i].protection += simulation.disease.vaccineEfficacy;
+    vaccPeople[i].transmission -= simulation.disease.vaccineEfficacy;
   }
 }
 
@@ -347,11 +347,11 @@ function getVaccLevel(vaccRate) {
 //Returns the infectiousness of the disease (refer to diseaseDictionary) according to disease
 //Pre-condition: disease is the name of a disease inputed by the user
 function getDiseaseLevel(disease) {
-  if(disease == "Covid" || disease == "covid") {
+  if(disease == "Covid") {
     return diseaseDictionary.leastInfectious;
-  } else if(disease == "Rubella" || disease == "rubella") {
+  } else if(disease == "Rubella") {
     return diseaseDictionary.mediumInfectious;
-  } else if(disease == "Measles" || disease == "measles") {
+  } else if(disease == "Measles") {
     return diseaseDictionary.mostInfectious;
   }
 }
@@ -372,7 +372,7 @@ function display(dayInfo) {
 
       if (dayGrid[i][j].immuneStatus) {
         context.fillStyle = "green";
-      } else if (dayGrid[i][j].infectStatus && dayGrid[i][j].timeInfect >= simulation.disease.daysToSx) {
+      } else if (dayGrid[i][j].infectStatus && dayGrid[i][j].timeInfect >= simulation.disease.daysToSymptoms) {
         context.fillStyle = "red";
       } else if (dayGrid[i][j].infectStatus) {
         context.fillStyle = "orange"; 
@@ -386,8 +386,8 @@ function display(dayInfo) {
   }
 
   dayDisplay.innerHTML = `Day ${day}`;
-  infectedDisplay.innerHTML = `Infected: ${dayInfo.infectNum}`;
-  immuneDisplay.innerHTML = `Immune: ${dayInfo.immuneNum}`;
+  infectedDisplay.innerHTML = `Infected: ${dayInfo.prevalence}`;
+  immuneDisplay.innerHTML = `Immune: ${dayInfo.resistant}`;
 }
 
 //Deletes the current cavas elements in the grid
@@ -430,8 +430,10 @@ function simulate() {
     simulation.days[day] = {
       day: day+1,
       grid: JSON.parse(JSON.stringify(town.grid)),
-      infectNum: totalInfected,
-      immuneNum: totalImmune
+      prevalence: totalInfected,
+      incidence: totalInfected - simulation.days[day-1].prevalence,
+      resistant: totalImmune,
+      r: 0
     };
   
     day++;
@@ -444,6 +446,93 @@ function simulate() {
   day = 1;
   maxDay = simulation.days.length;
   maxDayDisplay.innerHTML = `The simulation continues until Day ${maxDay}.`;
+}
+
+//Desc : creates a line graph according to data
+//Pre  : data is an array of objects
+function graph(data) {
+  data.forEach(function (d) {
+    d.infected = d.prevalence;
+    d.date = d.day;
+  })
+  // append the svg object to the body of the page
+  var svg = d3.select("#graph")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([ 0, maxDay ])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain( [ 0, 100 ])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add the line
+  svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "#69b3a2")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .curve(d3.curveBasis)
+      .x(function (d) {
+        return x(d.date);
+      })
+      .y(function (d) {
+        return y(d.infected);
+      })
+  )
+
+  // create a tooltip
+  var Tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px");
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1);
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html("Exact value: " + d.infected)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px");
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0);
+  }
+
+  // Add the points
+  svg
+    .append("g")
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return x(d.date) } )
+    .attr("cy", function(d) { return y(d.infected) } )
+    .attr("r", 5)
+    .attr("fill", "#69b3a2")
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
 }
 
 //Deletes the current canvas elements in the grid, updates json (simulation) variables according to user input,
@@ -459,8 +548,9 @@ function runUserSim() {
   simulation.patientZeroPosition = [7, 5];
   simulation.disease = getDiseaseLevel(diseaseInput.value);
   simulation.maskLevel = getMaskLevel(maskInput.value);
-  simulation.maskProtection = 10;
+  simulation.maskProtection = 20;
   simulation.vaccLevel = getVaccLevel(vaccInput.value);
+  jsonInput.value = JSON.stringify(simulation, null, " ");
 
   for (var i = 0; i < simulation.populationSize; i++) {
     totalPopulation[i].transmission = 50;
@@ -471,8 +561,10 @@ function runUserSim() {
     totalPopulation[i].timeInfect = 0;
     totalPopulation[i].immuneStatus = false;
   }
-  
   simulate();
+
+  d3.select("svg").remove(); //clearing previous graph
+  graph(simulation.days.slice(0, day));
 }
 
 //Deletes the current canvas elements in the grid, updates json (simulation) variables according to the json
@@ -491,6 +583,7 @@ function runConfigSim() {
   simulation.maskLevel = tempSim.maskLevel;
   simulation.maskProtection = tempSim.maskProtection;
   simulation.vaccLevel = tempSim.vaccLevel;
+  jsonInput.value = JSON.stringify(simulation, null, " ");
 
   for (var i = 0; i < simulation.populationSize; i++) {
     totalPopulation[i].transmission = 50;
@@ -501,8 +594,10 @@ function runConfigSim() {
     totalPopulation[i].timeInfect = 0;
     totalPopulation[i].immuneStatus = false;
   }
-  
   simulate();
+
+  d3.select("svg").remove(); //clearing previous graph
+  graph(simulation.days.slice(0, day));
 }
 
 //Decrements day and draws the grid of the corresponding day
@@ -521,6 +616,12 @@ function addDay() {
   }
   day++;
   display(simulation.days[day-1]);
+
+  d3.select("svg").remove(); //clearing previous graph
+  if(dayReached < day) {
+    dayReached = day;
+  }
+  graph(simulation.days.slice(0, dayReached));
 }
 
 // Desc : makes the list of people 
@@ -532,9 +633,17 @@ for (var i = 0; i < simulation.populationSize; i++) {
 // Desc : declaring variables needed for the simulation
 const town = new Grid(simulation.gridHeight, simulation.gridWidth);
 var day = 1;
+var dayReached = day;
 var day1Data = {
-  day: day,
+  day: 1,
   grid: JSON.parse(JSON.stringify(town.grid)),
-  infectNum: 1,
-  immuneNum: 0
+  prevalence: 1,
+  incidence: 0, 
+  resistant: 0,
+  r: 0
 };
+
+// set the dimensions and margins of the graph
+var margin = {top: 60, right: 90, bottom: 30, left: 30},
+width = 560 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
